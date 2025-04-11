7314917623119708d7d90e10a225bb4608e2b969
Tobias Payreder <t.payreder@students.htl-leonding.ac.at>
2025-04-11 08:57:41 +0200

    stying

--- project/script/ShopJS.js
+++ project/script/ShopJS.js
@@ -24,13 +24,13 @@ function loadNav() {
     let nav = document.querySelector('nav');
 
     nav.innerHTML = `
-        <div class="logo"><img src=".${MUST_HAVE_DATA.nav[1].link}" onclick="openLoginWindow()"><a id="logo" href=".${MUST_HAVE_DATA.nav[0].link}">MindQuest</a></div>
+        <div class="logo"><img class="loginGuy" src=".${MUST_HAVE_DATA.nav[1].link}" onclick="openLoginWindow()"><a id="logo" href=".${MUST_HAVE_DATA.nav[0].link}">MindQuest</a></div>
         <div class="nav"></div>
     `;
 
     for (let i = 2; i < MUST_HAVE_DATA.nav.length - 1; i++) {
         document.getElementsByClassName('nav')[0].innerHTML += `
-            <a href="${MUST_HAVE_DATA.nav[i].link}">${MUST_HAVE_DATA.nav[i].name}</a>
+            <a href=".${MUST_HAVE_DATA.nav[i].link}">${MUST_HAVE_DATA.nav[i].name}</a>
         `;
     }
 
@@ -111,6 +111,7 @@ function loadDarkWhiteMode() {
         root.style.setProperty('--background-image', 'linear-gradient(320deg, #fff4fa 0%, #fde0f0 50%, #fcd6e9 100%)');
         root.style.setProperty('--nav-gradient', 'linear-gradient(40deg, #f4b4d7 0%, #f28ec4 50%, #fcb4dc 100%)');
         root.style.setProperty('--extra-highlight', '#f9abd7');
+        root.style.setProperty('--invert', 'invert(0)');
 
         localStorage.setItem('theme', 'WhiteMode');
 
@@ -123,6 +124,7 @@ function loadDarkWhiteMode() {
         root.style.setProperty('--background-image', 'linear-gradient(320deg, #1b0f1b 0%, #2a1a2e 50%, #1b0f1b 100%)');
         root.style.setProperty('--nav-gradient', 'linear-gradient(40deg, #3a1c2f 0%, #5e2a4d 50%, #3a1c2f 100%)');
         root.style.setProperty('--extra-highlight', '#51213c');
+        root.style.setProperty('--invert', 'invert(1)');
 
 
         localStorage.setItem('theme', 'DarkMode');
@@ -143,6 +145,7 @@ function changeDarkWhiteMode() {
         root.style.setProperty('--background-image', 'linear-gradient(320deg, #1b0f1b 0%, #2a1a2e 50%, #1b0f1b 100%)');
         root.style.setProperty('--nav-gradient', 'linear-gradient(40deg, #3a1c2f 0%, #5e2a4d 50%, #3a1c2f 100%)');
         root.style.setProperty('--extra-highlight', '#51213c');
+        root.style.setProperty('--invert', 'invert(1)');
 
         localStorage.setItem('theme', 'DarkMode');
 
@@ -155,6 +158,7 @@ function changeDarkWhiteMode() {
         root.style.setProperty('--background-image', 'linear-gradient(320deg, #fff4fa 0%, #fde0f0 50%, #fcd6e9 100%)');
         root.style.setProperty('--nav-gradient', 'linear-gradient(40deg, #f4b4d7 0%, #f28ec4 50%, #fcb4dc 100%)');
         root.style.setProperty('--extra-highlight', '#f9abd7');
+        root.style.setProperty('--invert', 'invert(0)');
 
         localStorage.setItem('theme', 'WhiteMode');
 
--- project/script/StartpageJS.js
+++ project/script/StartpageJS.js
@@ -48,6 +48,7 @@ function loadDarkWhiteMode() {
         root.style.setProperty('--background-image', 'linear-gradient(320deg, #fff4fa 0%, #fde0f0 50%, #fcd6e9 100%)');
         root.style.setProperty('--nav-gradient', 'linear-gradient(40deg, #f4b4d7 0%, #f28ec4 50%, #fcb4dc 100%)');
         root.style.setProperty('--extra-highlight', '#f9abd7');
+        root.style.setProperty('--invert', 'invert(1)');
 
         localStorage.setItem('theme', 'WhiteMode');
 
@@ -60,6 +61,7 @@ function loadDarkWhiteMode() {
         root.style.setProperty('--background-image', 'linear-gradient(320deg, #1b0f1b 0%, #2a1a2e 50%, #1b0f1b 100%)');
         root.style.setProperty('--nav-gradient', 'linear-gradient(40deg, #3a1c2f 0%, #5e2a4d 50%, #3a1c2f 100%)');
         root.style.setProperty('--extra-highlight', '#51213c');
+        root.style.setProperty('--invert', 'invert(0)');
 
 
         localStorage.setItem('theme', 'DarkMode');
@@ -79,6 +81,7 @@ function changeDarkWhiteMode() {
         root.style.setProperty('--background-image', 'linear-gradient(320deg, #1b0f1b 0%, #2a1a2e 50%, #1b0f1b 100%)');
         root.style.setProperty('--nav-gradient', 'linear-gradient(40deg, #3a1c2f 0%, #5e2a4d 50%, #3a1c2f 100%)');
         root.style.setProperty('--extra-highlight', '#f9abd7');
+        root.style.setProperty('--invert', 'invert(0)');
         
         localStorage.setItem('theme', 'DarkMode');
 
@@ -91,6 +94,7 @@ function changeDarkWhiteMode() {
         root.style.setProperty('--background-image', 'linear-gradient(320deg, #fff4fa 0%, #fde0f0 50%, #fcd6e9 100%)');
         root.style.setProperty('--nav-gradient', 'linear-gradient(40deg, #f4b4d7 0%, #f28ec4 50%, #fcb4dc 100%)');
         root.style.setProperty('--extra-highlight', '#51213c');
+        root.style.setProperty('--invert', 'invert(1)');
 
 
         localStorage.setItem('theme', 'WhiteMode');
@@ -109,7 +113,7 @@ let nav = document.querySelector('nav');
 
 function loadNav() {
     nav.innerHTML = `
-        <div class="logo"><img src="${MUST_HAVE_DATA.nav[1].link}"><a id="logo" href="${MUST_HAVE_DATA.nav[0].link}">MindQuest</a></div>
+        <div class="logo"><img class="loginGuy" src="${MUST_HAVE_DATA.nav[1].link}"><a id="logo" href="${MUST_HAVE_DATA.nav[0].link}">MindQuest</a></div>
         <div class="nav"></div>
     `;
 
